1ef2bf71a8e0d4bf64e928ce4b1162528d40c2d8
Stanislav Malík <stanislav.malik@student.tuke.sk>
2024-04-21 13:47:02 +0200

    hide page for user testing from menu

--- assets/i18n.6a77cddd.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index.04e7caea.js";import{i as a}from"./index.392eb83b.js";var m=r(({app:o})=>{o.use(a)});export{m as default};
---
+++ assets/i18n.79db6e36.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index.7a9aee17.js";import{i as a}from"./index.1dc78791.js";var m=r(({app:o})=>{o.use(a)});export{m as default};
